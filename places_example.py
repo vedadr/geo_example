fd1c83eed7ae2efef2512550341aa228cf5e3b0f
vedadr <vedadr@gmail.com>
2015-12-13 12:21:31 +0100

    message

---
+++ places_example.py
@@ -0,0 +1,26 @@
+# geocoding example with third party package
+# from geopy import geocoders
+#
+# g = geocoders.GoogleV3()
+# place, (lat, lng) = g.geocode("Olimpijska 26, sarajevo")
+# print("{} {} {}".format(place, lat, lng))
+#
+
+# more info https://developers.google.com/places/web-service/search
+
+from urllib.request import urlopen # this package is needed just to download data from web location
+import json # this package we will use to easier decode json
+
+response = urlopen("https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
+                                  "location=43.855624,18.379420" # your location in lat long coordinates
+                                  "&radius=500" # radius in meters around coordinates to include in search
+                                  "&types=food" # type of venue, try with cafe, bank etc.
+                                  # "&name=slatko" # include for filtering by name
+                                  "&key=AIzaSyBIde_4tmXK7iajeV2eMCMcREwTKaboWNE") # key for identification, create your own
+
+jsonRaw = response.read().decode('utf-8') # this will read json from downloaded object
+jsonData = json.loads(jsonRaw)  # this will convert json into python object
+
+results = ','.join([result['name'] for result in jsonData['results']]) #ja dodao samo da ispiltriram nazive
+
+print('Rezultati: {}'.format(results))
